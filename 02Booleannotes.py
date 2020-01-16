f8da7bc52e1278fbc156131f1551e30bbdce9fc2
Engrid <engrid.hernandez@gmail.com>
2020-01-16 17:06:00 -0500

    added new notes

--- 02Booleannotes.py
+++ 02Booleannotes.py
@@ -2,3 +2,13 @@
 # Equals is == and Not Equals is !=
 # True and False are "bool" types
 # a boolean variable is a variable set to a bool (True or False)
+
+# Boolean operators combine smaller boolean expressions into a single exxpression.
+
+Three major boolean operators are as follows:
+	and
+		combines two boolean expressions and evaluates as True only if both expressions are True
+	or
+		combines two boolean expressions and evaluates as True as long as one of the expressions is true
+	not
+		combines two boolean expressions and evaluates as True
