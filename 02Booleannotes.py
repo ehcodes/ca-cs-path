ad1a28cfac3cbd4c679d3f9c4957ec64a153385c
Engrid <engrid.hernandez@gmail.com>
2020-01-16 15:53:41 -0500

    Not taking on Relational Operators in python

---
+++ 02Booleannotes.py
@@ -0,0 +1,4 @@
+# True or False is returned when using relational operators which cocmpare two values.
+# Equals is == and Not Equals is !=
+# True and False are "bool" types
+# a boolean variable is a variable set to a bool (True or False)
